86e478d0f89368f65cbf7f4ca0af6e3b3211eaa9
ktnoc <55606162+ktnoc@users.noreply.github.com>
2020-04-06 00:19:52 +0200

    init

---
+++ setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="ktbastion",
+    version="0.0.1",
+    author="ktnoc",
+    author_email="ktnoc@users.noreply.github.com",
+    description="KTBastion",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/ktnoc/ktbastion",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
